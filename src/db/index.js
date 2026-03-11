5c289d1dd16c2c1217a5735ae320d5fb69741560
Aryaman-90 <aryamansharma5360@gmail.com>
2026-03-11 13:00:16 +0530

    DB connection Successfull

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose  from "mongoose";
+import dotenv from "dotenv";
+import { DB_NAME } from "../constants.js";
+
+
+
+
+
+export const connectDB = async () => {
+    try {
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       console.log("MongoDB connected : ", connectionInstance.connection.host);
+       return connectionInstance;
+    } catch (error) {
+        console.error("ERROR : ", error)
+        throw error
+    }
+} 
