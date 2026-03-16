cb085434a3bfb177bbf6efaf84f1a92684852a21
Aryaman-90 <aryamansharma5360@gmail.com>
2026-03-16 10:19:58 +0530

    All setup completed ( video 12)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";   
 import cors from "cors";
-
+const app = express();
 app.use(cors({
-    origin: Process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
 
@@ -12,6 +12,14 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"));
 app.use(cookieParser());
-const app = express();
+
+
+
+//routes
+
+import userRoutes from "./routes/user.routes.js";
+
+//route declaration
+app.use("/api/v1/users", userRoutes);
 
 export{app};
